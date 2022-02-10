b175f44bcf957e0dfe4c063900708f7db3c63232
salangdung-i <dladmswl92@naver.com>
2022-02-10 19:00:11 +0900

    aysnc/await 적용

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ const App = () => {
 
   // onClick 함수에서 axios.get 함수를 사용했다. 
   // 이 함수는 파라미터로 전달된 주소에 get요청을 해준다. 이에 대한 결과는 .then을 통해 비동기적으로 확인 할 수 있다.
-  const onClick = () => {
-    axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/todos/1'
+      );
       setData(response.data);
-    });
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
