797dbfc9805af475678c3a8454d93ce2b2b44c85
salangdung-i <dladmswl92@naver.com>
2022-03-16 18:03:51 +0900

    카테고리 레이아웃 구현

---
+++ src/components/categories/categories.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styles from '../categories/categories.module.css';
+
+const categories = [
+  {
+    name: 'all',
+    text: '종합'
+  },
+  {
+    name: 'business',
+    text: '경제'
+  },
+  {
+    name: 'entertainment',
+    text: '방송/통신'
+  },
+  {
+    name: 'general',
+    text: '일반'
+  },
+  {
+    name: 'health',
+    text: '건강'
+  },
+  {
+    name: 'science',
+    text: '과학'
+  },
+  {
+    name: 'sports',
+    text: '스포츠'
+  },
+  {
+    name: 'technology',
+    text: '기술'
+  }
+];
+
+const Categories = () => {
+  return (
+    <div className={styles.categories}>
+      {categories.map(category => <div className={styles.category}>{category.text}</div>)}
+    </div>
+  );
+}
+
+export default Categories;
