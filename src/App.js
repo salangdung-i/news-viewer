4794863d9b6a0eb7bf482b795c9098ea0513bf45
salangdung-i <dladmswl92@naver.com>
2022-03-17 12:54:15 +0900

    라우터 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import NewsPage from './components/newsPage/newsPage';
+import { Route } from 'react-router';
+import { Routes } from '../node_modules/react-router/index';
 
 
-const App = () => {
+const App = ({ newsService }) => {
   return (
-    <h1>React</h1>
+    <Routes >
+      <Route path="/" element={<NewsPage newsService={newsService} />} />
+      <Route path="/:category" element={<NewsPage newsService={newsService} />} />
+    </Routes >
   );
-};
+}
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import NewsService from './services/newsService';
 import { BrowserRouter } from '../node_modules/react-router-dom/index';
 
+
+const newsService = new NewsService();
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <App newsService={newsService} />
   </BrowserRouter>,
   document.getElementById('root')
 );
